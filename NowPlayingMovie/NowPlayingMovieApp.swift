a59c4bbe5acc27c92a4a0dec7f32bb2dc0f2ac6b
Taekyun Kim <taekkim@student.42seoul.kr>
2023-06-07 00:38:28 +0900

    Initial Commit

---
+++ NowPlayingMovie/NowPlayingMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowPlayingMovieApp.swift
+//  NowPlayingMovie
+//
+//  Created by taekkim on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct NowPlayingMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
